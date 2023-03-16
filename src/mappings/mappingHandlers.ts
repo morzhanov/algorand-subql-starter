e7ab92b9bf2c5ddac98dc6a2678a1a1c109c5ac2
Vlad Morzhanov <vlad.morzhanov@gmail.com>
2023-03-16 12:45:07 +0200

    remove addresses table and update field names

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,29 +1,18 @@
 import { AlgorandTransaction } from "@subql/types-algorand";
-import { Transaction, Address } from "../types";
+import { Transaction } from "../types";
 
 export async function handleTransaction(
   tx: AlgorandTransaction
 ): Promise<void> {
   // logger.info(JSON.stringify(tx));
   if (tx.assetTransferTransaction) {
-    // ensure that our address entities exist
-    const senderAddress = await Address.get(tx.sender.toLowerCase());
-    if (!senderAddress) {
-      await new Address(tx.sender.toLowerCase()).save();
-    }
-
-    const txReceiverAddr = tx.assetTransferTransaction.receiver.toLowerCase()
-    const receiverAddress = await Address.get(txReceiverAddr);
-    if (!receiverAddress) {
-      await new Address(txReceiverAddr).save();
-    }
     // Create the new transfer entity
     const transactionEntity: Transaction = Transaction.create({
       id: tx.id,
-      blockHeight: tx.confirmedRound,
-      senderId: tx.sender.toLowerCase(),
-      receiverId: txReceiverAddr,
-      blockTime: tx.roundTime,
+      blockNumber: tx.confirmedRound,
+      blockTimestamp: tx.roundTime,
+      from: tx.sender.toLowerCase(),
+      to: tx.assetTransferTransaction.receiver.toLowerCase(),
       amount: BigInt(tx.assetTransferTransaction.amount),
     });
     await transactionEntity.save();
